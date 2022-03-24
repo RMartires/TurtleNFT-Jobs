2d10337dc7a97eb3442d4a4a614460be3e59db0c
RMartires <rohitmartires14@gmail.com>
2022-03-25 00:19:50 +0530

    fix: verifyWebhook

--- utill/verifyWebhook.js
+++ utill/verifyWebhook.js
@@ -5,8 +5,9 @@ require('dotenv').config();
 const SHOPIFY_API_SECRET = process.env.SHOPIFY_API_SECRET;
 
 exports.verifyWebhook = async (req, res, buf, encoding) => {
+    let url = req.url.split("/jobs")[1];
     if (["/ordersCreate", "/uninstall", "/MWCustomerData",
-        "/MWCustomerErasure", "/MWShopErasure"].includes(req.url)) {
+        "/MWCustomerErasure", "/MWShopErasure"].includes(url)) {
         const hmac = req.get("X-Shopify-Hmac-Sha256");
         const hash = crypto
             .createHmac("sha256", SHOPIFY_API_SECRET)
