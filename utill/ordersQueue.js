bc7bde7ecada4ec879ad55131426c8f9cb16137f
RMartires <rohitmartires14@gmail.com>
2022-06-16 03:06:20 +0530

    fix: orderQueu

--- utill/ordersQueue.js
+++ utill/ordersQueue.js
@@ -159,8 +159,8 @@ async function createOrder(jobData, orderId, orderStatus) {
             shop: contracts[idx].shop,
             blockchain: contracts[idx].blockchain,
             RandomId: contracts[idx].RandomId || false,
-            type: contracts[idx]?.type,
-            biconomy: contracts[idx]?.biconomy,
+            type: contracts[idx]?.type || false,
+            biconomy: contracts[idx]?.biconomy || false,
         });
     });
     let UUID = v4();
